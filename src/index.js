3e529082ae4a7ac481b53632d68b900ebf5f6cdd
namkiyoun <skarldbs321@gmail.com>
2023-07-12 21:28:51 +0900

    리액트 공부/실습

--- src/index.js
+++ src/index.js
@@ -3,12 +3,27 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createRoot} from "react-dom/client";
+import Library from './chapter_03/Library';
+import Clock from './chapter_04/Clock';
+import CommentList from './chapter_05/CommentList';
+import NotificationList from './chapter_06/NotificationList';
+import Accommodate from './chapter_07/Accommodate';
+import ConfirmButton from './chapter_08/ConfirmButton';
+import LandingPage from './chapter_09/LandingPage';
+import AttendanceBook from './chapter_10/AttendanceBook';
+import SignUp from './chapter_11/SignUp';
+import Calculator from './chapter_12/Calculator';
+import ProfileCard from './chapter_13/ProfileCard';
+import DarkOrLight from './chapter_14/DarkOrLight';
+import Blocks from './chapter_15/Blocks';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+
+const rootNode = document.getElementById('root');
+ReactDOM.createRoot(rootNode).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Blocks />
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
